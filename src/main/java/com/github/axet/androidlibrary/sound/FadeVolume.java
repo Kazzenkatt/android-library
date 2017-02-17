023a44ee1363ac9c1c8b84fe7847a4ee9e8b789e
Alexey Kuznetsov <axet@me.com>
2017-02-17 10:07:34 +0300

    simply fade

--- src/main/java/com/github/axet/androidlibrary/sound/FadeVolume.java
+++ src/main/java/com/github/axet/androidlibrary/sound/FadeVolume.java
@@ -3,25 +3,16 @@ package com.github.axet.androidlibrary.sound;
 import android.os.Handler;
 
 public class FadeVolume implements Runnable {
-    Handler handler = new Handler();
+    public Handler handler = new Handler();
 
     public int step = 0;
     public int steps = 50;
     public int delay = 100;
-    // we start from startVolume, rest - how much we should increase
-    public float rest = 0;
-    public float startVolume = 0;
-    public float inc;
 
-    public FadeVolume(int sec, int inc) {
-        this(inc < 0 ? 1 : 0, sec, inc);
-    }
-
-    public FadeVolume(float startVolume, int sec, int inc) {
-        this.startVolume = startVolume;
-        this.inc = inc;
-        this.steps = (sec / delay);
-        this.rest = 1f - startVolume;
+    public FadeVolume(int sec) {
+        steps = (sec / delay);
+        if (steps < 1)
+            steps = 1;
     }
 
     public void stop() {
@@ -36,10 +27,8 @@ public class FadeVolume implements Runnable {
         // volume 0..1
         float vol = 1 - log1;
 
-        // actual volume
-        float restvol = startVolume + inc * rest * vol;
-
-        step(restvol);
+        if (!step(vol))
+            return;
 
         step++;
 
@@ -53,7 +42,8 @@ public class FadeVolume implements Runnable {
         handler.postDelayed(this, delay);
     }
 
-    public void step(float vol) {
+    public boolean step(float vol) {
+        return true;
     }
 
     public void done() {
