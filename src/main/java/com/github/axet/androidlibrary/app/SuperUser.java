310bb421e9a8c43e8d30ab4d5511e57603e24577
Alexey Kuznetsov <axet@me.com>
2017-06-27 00:04:55 +0300

    expanding openfiledialog

--- src/main/java/com/github/axet/androidlibrary/app/SuperUser.java
+++ src/main/java/com/github/axet/androidlibrary/app/SuperUser.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -19,20 +21,24 @@ public class SuperUser {
     public static final String BIN_TRUE = "/usr/bin/true";
     public static final String BIN_REBOOT = "/system/bin/reboot";
 
-    public static void su(String cmd) {
+    public static final String KILL = " || kill -9 $$"; // some su does not return error codes in scripts, kill it
+
+    public static int su(String cmd) {
         try {
             Process su = Runtime.getRuntime().exec(BIN_SU);
             DataOutputStream os = new DataOutputStream(su.getOutputStream());
-            os.writeBytes(cmd + "\n");
+            os.writeBytes(cmd + KILL + "\n");
             os.flush();
             os.writeBytes("exit\n");
             os.flush();
             su.waitFor();
+            return su.exitValue();
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
+        return -1;
     }
 
     public static void reboot() {
@@ -72,5 +78,34 @@ public class SuperUser {
     public static boolean isReboot() {
         File f2 = new File(BIN_REBOOT);
         return isRooted() && f2.exists();
+    }
+
+    public static String escapePath(String p) {
+        p = p.replaceAll(" ", "\\ "); // ' ' -> '\'
+        p = p.replaceAll("\"", "\\\""); // '"' -> '\"'
+        return p;
+    }
+
+    public static boolean touch(File f) {
+        String p = f.getAbsolutePath();
+        return su("touch -a " + escapePath(p)) == 0;
+    }
+
+    public static boolean mkdirs(File f) {
+        String p = f.getAbsolutePath();
+        return su("mkdir -p " + escapePath(p)) == 0;
+    }
+
+    public static boolean delete(File f) {
+        String p = f.getAbsolutePath();
+        return su("rm -rf " + escapePath(p)) == 0;
+    }
+
+    public static boolean mv(File f, File to) {
+        String p1 = escapePath(f.getAbsolutePath());
+        String pp = p1 + " " + escapePath(to.getAbsolutePath());
+        String mv = "mv " + pp;
+        String cp = "cp " + pp + " && rm " + p1;
+        return su(mv + " || " + cp) == 0;
     }
 }
