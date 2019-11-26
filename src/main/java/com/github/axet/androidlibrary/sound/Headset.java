5a6de2e882818e1823a53b64b8dbd68ec9dd95e3
Alexey Kuznetsov <axet@me.com>
2019-11-26 22:36:11 +0300

    rename headset methods

--- src/main/java/com/github/axet/androidlibrary/sound/Headset.java
+++ src/main/java/com/github/axet/androidlibrary/sound/Headset.java
@@ -11,11 +11,12 @@ import android.util.Log;
 public class Headset {
     public static String TAG = Headset.class.getSimpleName();
 
-    public static int FLAGS = MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_QUEUE_COMMANDS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS;
     public static long ACTIONS_MAIN = PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PAUSE | PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_STOP;
     public static long ACTIONS_SKIP = PlaybackStateCompat.ACTION_SKIP_TO_NEXT | PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS;
-    public static long ACTIONS = ACTIONS_MAIN | ACTIONS_SKIP;
+
     public MediaSessionCompat msc;
+    public long actions = ACTIONS_MAIN | ACTIONS_SKIP;
+    public int flags = MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_QUEUE_COMMANDS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS;
 
     public static void handleIntent(Headset headset, Intent intent) {
         if (headset == null)
@@ -23,7 +24,10 @@ public class Headset {
         MediaButtonReceiver.handleIntent(headset.msc, intent);
     }
 
-    public Headset(Context context, Class cls) {
+    public Headset() {
+    }
+
+    public void create(Context context, Class cls) {
         Log.d(TAG, "headset mediabutton on");
         ComponentName name = new ComponentName(context, cls);
         msc = new MediaSessionCompat(context, TAG, name, null);
@@ -53,14 +57,14 @@ public class Headset {
                 Headset.this.onSkipToPrevious();
             }
         });
-        msc.setFlags(FLAGS);
+        msc.setFlags(flags);
         msc.setActive(true);
         msc.setPlaybackState(new PlaybackStateCompat.Builder().setState(PlaybackStateCompat.STATE_PLAYING, 0, 1).build()); // bug, when after device reboots we have to set playing state to 'playing' to make mediabutton work
     }
 
     public void setState(boolean playing) {
         PlaybackStateCompat.Builder builder = new PlaybackStateCompat.Builder()
-                .setActions(ACTIONS)
+                .setActions(actions)
                 .setState(playing ? PlaybackStateCompat.STATE_PLAYING : PlaybackStateCompat.STATE_PAUSED, 0, 1);
         msc.setPlaybackState(builder.build());
     }
@@ -80,7 +84,7 @@ public class Headset {
     public void onSkipToPrevious() {
     }
 
-    public void release() {
+    public void close() {
         Log.d(TAG, "headset mediabutton off");
         msc.release();
         msc = null;
