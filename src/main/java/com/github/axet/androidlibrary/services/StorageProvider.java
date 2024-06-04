3b682efb55b0a514801c000ebaaf7845104cf5b7
Alexey Kuznetsov <axet@me.com>
2024-06-04 15:24:18 +0300

    allow to write storage files

--- src/main/java/com/github/axet/androidlibrary/services/StorageProvider.java
+++ src/main/java/com/github/axet/androidlibrary/services/StorageProvider.java
@@ -273,6 +273,9 @@ public class StorageProvider extends ContentProvider {
         public void close() throws IOException {
             is.close();
         }
+
+        public void onClose(File tmp, IOException e) { // SDK19+ / RW only
+        }
     }
 
     public static class ParcelInputStream extends ParcelFileDescriptor {
@@ -701,7 +704,17 @@ public class StorageProvider extends ContentProvider {
                         Log.d(TAG, "copy close error", e);
                     }
                 }
-                return ParcelFileDescriptor.open(tmp, FileProvider.modeToMode(mode));
+                if (Build.VERSION.SDK_INT >= 19) {
+                    final File finalTmp = tmp;
+                    return ParcelFileDescriptor.open(tmp, FileProvider.modeToMode(mode), handler, new ParcelFileDescriptor.OnCloseListener() {
+                        @Override
+                        public void onClose(IOException e) {
+                            is.onClose(finalTmp, e);
+                        }
+                    });
+                } else {
+                    return ParcelFileDescriptor.open(tmp, FileProvider.modeToMode(mode));
+                }
             }
         } catch (IOException e) {
             throw fnfe(e);
